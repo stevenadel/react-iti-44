c4541938457f969302497acb30040de969cfebca
Steven Adel <stevenadel.dev@gmail.com>
2024-03-19 10:33:44 +0200

    Lab 3 Products: add link to products list

--- Lab 3/products-react/src/pages/ProductDetails/ProductDetails.jsx
+++ Lab 3/products-react/src/pages/ProductDetails/ProductDetails.jsx
@@ -1,6 +1,7 @@
 import { useParams } from 'react-router-dom'
 import { Container, Row, Col, Image } from 'react-bootstrap'
 import productsJSON from "../../assets/products.json"
+import { Link } from "react-router-dom"
 import "./ProductDetails.css"
 
 export default function ProductDetails() {
@@ -10,21 +11,26 @@ export default function ProductDetails() {
     const product = products.find(prod => prod.id == productId)
 
     return (
-        <Container>
-            <Image src={product.thumbnail} fluid  className="w-75 main-img"/>
-            <Row className='my-3 align-items-center'>
-                {product.images.map((img, index) => (
-                    <Col key={index}>
-                        <Image src={img} fluid className="sub-img" />
-                    </Col>
-                ))}
-            </Row>
-            <Row>
-                <h1 className="mt-3">{product.title}</h1>
-                <p className="mb-3 text-secondary">{product.description}</p>
-                <h2 className="my-2">{product.brand}</h2>
-                <h3>{product.category}</h3>
-            </Row>
-        </Container>
+        <>
+            <Container>
+                <Row>
+                    <Link to="/" className="prod-link mb-4">Products List</Link>
+                </Row>
+                <Image src={product.thumbnail} fluid className="w-75 main-img" />
+                <Row className='my-3 align-items-center'>
+                    {product.images.map((img, index) => (
+                        <Col key={index}>
+                            <Image src={img} fluid className="sub-img" />
+                        </Col>
+                    ))}
+                </Row>
+                <Row>
+                    <h1 className="mt-3">{product.title}</h1>
+                    <p className="mb-3 text-secondary">{product.description}</p>
+                    <h2 className="my-2">{product.brand}</h2>
+                    <h3>{product.category}</h3>
+                </Row>
+            </Container>
+        </>
     )
 }
